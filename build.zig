14fac59c7c077d1b471655c700a990d880881de3
Mike <mike@prime31.com>
2022-03-09 13:09:53 -0800

    submodules fix

--- build.zig
+++ build.zig
@@ -8,44 +8,86 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("mach-glfw-vulkan-example", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+    const examples = getAllExamples(b, "examples");
+    for (examples) |example| {
+        if (std.mem.endsWith(u8, example[0], "chain") or std.mem.endsWith(u8, example[0], "context")) continue;
 
-    // optional: uncommenting will create the vk.zig file in the zig-out folder. It should be copied to the root folder.
-    // vulkan-zig: Create a step that generates vk.zig (stored in zig-out) from the provided vulkan registry.
-    // const gen = vkgen.VkGenerateStep.initFromSdk(b, "/Users/mikedesaro/VulkanSDK/1.3.204.1/macOS", "vk.zig");
-    // exe.addPackage(gen.package);
-    exe.addPackage(.{
-        .name = "vulkan",
-        .path = .{
-            .path = std.fs.path.join(b.allocator, &[_][]const u8{
-                b.build_root,
-                b.cache_root,
-                "vk.zig",
-            }) catch unreachable,
-        },
-    });
+        const name = example[0];
+        const source = example[1];
 
-    // mach-glfw
-    exe.addPackagePath("glfw", "libs/mach-glfw/src/main.zig");
-    glfw.link(b, exe, .{});
+        var exe = b.addExecutable(name, source);
+        exe.setOutputDir("zig-cache/bin");
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+        exe.install();
 
-    // shader resources, to be compiled using glslc
-    const res = zigvulkan.ResourceGenStep.init(b, "resources.zig");
-    res.shader_step = vkgen.ShaderCompileStep.init(b, &[_][]const u8{ "glslc", "--target-env=vulkan1.3" }, "shaders");
-    res.step.dependOn(&res.shader_step.step);
-    res.addShader("triangle_vert", "shaders/triangle.vert");
-    res.addShader("triangle_frag", "shaders/triangle.frag");
-    exe.addPackage(res.package);
+        exe.addPackage(.{
+            .name = "vulkan",
+            .path = .{
+                .path = std.fs.path.join(b.allocator, &[_][]const u8{
+                    b.build_root,
+                    b.cache_root,
+                    "vk.zig",
+                }) catch unreachable,
+            },
+        });
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        // mach-glfw
+        exe.addPackagePath("glfw", "libs/mach-glfw/src/main.zig");
+        glfw.link(b, exe, .{});
+
+        // shader resources, to be compiled using glslc
+        const res = zigvulkan.ResourceGenStep.init(b, "resources.zig");
+        res.shader_step = vkgen.ShaderCompileStep.init(b, &[_][]const u8{ "glslc", "--target-env=vulkan1.3" }, "shaders");
+        res.step.dependOn(&res.shader_step.step);
+        res.addShader("triangle_vert", "shaders/triangle.vert");
+        res.addShader("triangle_frag", "shaders/triangle.frag");
+        exe.addPackage(res.package);
+
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(name, b.fmt("run {s}.zig", .{name}));
+        run_step.dependOn(&run_cmd.step);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // generate vk.zig bindings
+    var generate_exe = b.addSystemCommand(&[_][]const u8{ "echo", "done" });
+    const exe_step = b.step("generate_vulkan_bindings", b.fmt("Generates the vk.zig file", .{}));
+    exe_step.dependOn(&generate_exe.step);
+
+    _ = vkgen.VkGenerateStep.initFromSdk(b, "/Users/mikedesaro/VulkanSDK/1.3.204.1/macOS", "vk.zig");
+}
+
+fn getAllExamples(b: *std.build.Builder, root_directory: []const u8) [][2][]const u8 {
+    var list = std.ArrayList([2][]const u8).init(b.allocator);
+
+    var recursor = struct {
+        fn search(alloc: std.mem.Allocator, directory: []const u8, filelist: *std.ArrayList([2][]const u8)) void {
+            var dir = std.fs.cwd().openDir(directory, .{ .iterate = true }) catch unreachable;
+            defer dir.close();
+
+            var iter = dir.iterate();
+            while (iter.next() catch unreachable) |entry| {
+                if (entry.kind == .File) {
+                    if (std.mem.endsWith(u8, entry.name, ".zig")) {
+                        const abs_path = std.fs.path.join(alloc, &[_][]const u8{ directory, entry.name }) catch unreachable;
+                        const name = std.fs.path.basename(abs_path);
+
+                        filelist.append([2][]const u8{ name[0 .. name.len - 4], abs_path }) catch unreachable;
+                    }
+                } else if (entry.kind == .Directory) {
+                    const abs_path = std.fs.path.join(alloc, &[_][]const u8{ directory, entry.name }) catch unreachable;
+                    search(alloc, abs_path, filelist);
+                }
+            }
+        }
+    }.search;
+
+    recursor(b.allocator, root_directory, &list);
+
+    return list.toOwnedSlice();
 }
