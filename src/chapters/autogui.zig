0678412d22617b1edcd697f2e608a47cd0d8ae67
Mike <mike@prime31.com>
2022-04-01 11:24:41 -0700

    autogui jank

--- src/chapters/autogui.zig
+++ src/chapters/autogui.zig
@@ -9,9 +9,22 @@ pub fn nullTerm(comptime str: []const u8) [:0]const u8 {
 }
 
 pub fn inspect(comptime T: type, ptr: *T) void {
+    ig.igPushID_Ptr(ptr);
+    defer ig.igPopID();
+
+    inspectStruct(T, ptr, null);
+}
+
+pub fn inspectStruct(comptime T: type, ptr: *T, name: ?[:0]const u8) void {
     switch (@typeInfo(T)) {
         .Struct => |info| {
-            inline for (info.fields) |field| inspectField(field.field_type, &@field(ptr, field.name), nullTerm(field.name));
+            const label = if (name) |n| n else @typeName(T);
+
+            if (ig.igCollapsingHeader_BoolPtr(label, null, ig.ImGuiTreeNodeFlags_None)) {
+                ig.igIndent(10);
+                defer ig.igUnindent(10);
+                inline for (info.fields) |field| inspectField(field.field_type, &@field(ptr, field.name), nullTerm(field.name));
+            }
         },
         .Pointer => inspect(std.meta.Child(T), ptr.*),
         else => @compileError("Invalid type passed to inspect: " ++ @typeName(T)),
@@ -35,13 +48,8 @@ fn inspectField(comptime FieldType: type, fieldPtr: anytype, name: [:0]const u8)
             _ = info;
         },
         .Struct => {
-            ig.igAlignTextToFramePadding();
-            const node_open = ig.igTreeNode_Str(name);
-            if (node_open) {
-                ig.igText("%s", @typeName(FieldType));
-                inspect(FieldType, fieldPtr);
-                ig.igTreePop();
-            }
+            // ig.igAlignTextToFramePadding();
+            inspectStruct(FieldType, fieldPtr, name);
         },
         else => {
             ig.igText("<TODO " ++ @typeName(FieldType) ++ ">@0x%p", fieldPtr);
